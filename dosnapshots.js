68a5f671f400508b4693a5477ac44312618e216f
Bryan Nelson <bryan.nelson@gmail.com>
2017-07-30 18:19:19 -0500

    initial commit - kicks off snapshot creation for all your droplets

---
+++ dosnapshots.js
@@ -0,0 +1,21 @@
+var privkey = require('./privkey.secret').privkey;
+var digitalocean = require('digitalocean');
+var client = digitalocean.client(privkey);
+
+client.droplets.list().then(function(droplets) {
+  
+  for(var droplet of droplets) {
+    console.log("Starting snapshot of " + droplet.name + "...");
+    client.droplets.snapshot(droplet.id);
+  }
+ 
+}).then(function() {
+  
+  console.log("All droplets have started a new snapshot.");
+
+}).catch(function(err) {
+  
+  console.log('sadface, error: ' + err);  
+
+});
+
